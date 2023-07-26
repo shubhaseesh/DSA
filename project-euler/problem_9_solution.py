8ea1988e85fede57b57bda2833579825cf408b3e
shubhaseesh <shubhaseesh09@gmail.com>
2023-07-26 15:33:24 +0530

    solution 9

--- project-euler/problem_9_solution.py
+++ project-euler/problem_9_solution.py
@@ -5,15 +5,15 @@ Question link[Problem 9](https://www.hackerrank.com/contests/projecteuler/challe
 
 # @ Todo optimise the code
 def pythagorean_triplet(n):
-    p = None
     for c in range(n//3, n//2):
-        for b in range((n-c)//2, c):
-            a = n - c - b
-            if (c*c) == (a*a) + (b*b):
-                p = a*b*c
-                break
-        if p:
-            return p
+        for b in range((n - c + 1) // 2,c):
+            a = n - b - c
+            if a*a + b*b == c*c:
+                return a * b * c
+    return -1
 
 
 print(pythagorean_triplet(12))
+print(pythagorean_triplet(30))
+print(pythagorean_triplet(56))
+print(pythagorean_triplet(121))
